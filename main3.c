8f056e2992829b4dd61a6fbd1e9a06bce8ccdbe2
akjeyaramaji <akjeyaramaji@protonmail.com>
2019-12-14 19:37:05 +0530

    My Diploma project

---
+++ main3.c
@@ -0,0 +1,520 @@
+/*
+ * main3.c
+ *
+ * Created: 2/23/2016 2:58:59 AM
+ *  Author: ajithkumar
+ */ 
+
+#define F_CPU 14745600
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+
+#include <math.h> //included to support power function
+#include "lcd.c"
+
+
+int count=0;
+
+void port_init();
+void timer5_init();
+void velocity(unsigned char, unsigned char);
+void motors_delay();
+volatile unsigned long int ShaftCountLeft = 0; //to keep track of left position encoder
+volatile unsigned long int ShaftCountRight = 0; //to keep track of right position encoder
+volatile unsigned int Degrees; //to accept angle in degrees for turning
+
+
+unsigned char ADC_Conversion(unsigned char);
+unsigned char ADC_Value;
+unsigned char flag = 0;
+unsigned char Left_white_line = 0;
+unsigned char Center_white_line = 0;
+unsigned char Right_white_line = 0;
+unsigned char Front_IR_Sensor=0;
+unsigned char flag1 = 0;
+unsigned char flag2 = 0;
+
+
+
+void servo1_pin_config (void)
+{
+	DDRB  = DDRB | 0x20;  //making PORTB 5 pin output
+	PORTB = PORTB | 0x20; //setting PORTB 5 pin to logic 1
+}
+
+//Configure PORTB 6 pin for servo motor 2 operation
+void servo2_pin_config (void)
+{
+	DDRB  = DDRB | 0x40;  //making PORTB 6 pin output
+	PORTB = PORTB | 0x40; //setting PORTB 6 pin to logic 1
+}
+
+void timer1_init(void)
+{
+ TCCR1B = 0x00; //stop
+ TCNT1H = 0xFC; //Counter high value to which OCR1xH value is to be compared with
+ TCNT1L = 0x01;	//Counter low value to which OCR1xH value is to be compared with
+ OCR1AH = 0x03;	//Output compare Register high value for servo 1
+ OCR1AL = 0xFF;	//Output Compare Register low Value For servo 1
+ OCR1BH = 0x03;	//Output compare Register high value for servo 2
+ OCR1BL = 0xFF;	//Output Compare Register low Value For servo 2
+ OCR1CH = 0x03;	//Output compare Register high value for servo 3
+ OCR1CL = 0xFF;	//Output Compare Register low Value For servo 3
+ ICR1H  = 0x03;	
+ ICR1L  = 0xFF;
+ TCCR1A = 0xAB; /*{COM1A1=1, COM1A0=0; COM1B1=1, COM1B0=0; COM1C1=1 COM1C0=0}
+ 					For Overriding normal port functionality to OCRnA outputs.
+				  {WGM11=1, WGM10=1} Along With WGM12 in TCCR1B for Selecting FAST PWM Mode*/
+ TCCR1C = 0x00;
+ TCCR1B = 0x0C; //WGM12=1; CS12=1, CS11=0, CS10=0 (Prescaler=256)
+}
+
+
+
+
+//Function to configure LCD port
+void lcd_port_config (void)
+{
+ DDRC = DDRC | 0xF7; //all the LCD pin's direction set as output
+ PORTC = PORTC & 0x80; // all the LCD pins are set to logic 0 except PORTC 7
+}
+
+//ADC pin configuration
+void adc_pin_config (void)
+{
+	DDRK = 0x00;
+	PORTK = 0x00;
+}
+
+//Function to configure ports to enable robot's motion
+void motion_pin_config (void) 
+{
+ DDRA = DDRA | 0x0F;
+ PORTA = PORTA & 0xF0;
+ DDRL = DDRL | 0x18;   //Setting PL3 and PL4 pins as output for PWM generation
+ PORTL = PORTL | 0x18; //PL3 and PL4 pins are for velocity control using PWM.
+}
+void buzzer_pin_config (void)
+{
+	DDRC = DDRC | 0x08;		//Setting PORTC 3 as outpt
+	PORTC = PORTC & 0xF7;		//Setting PORTC 3 logic low to turnoff buzzer
+}
+void buzzer_on (void)
+{
+	PORTC=0x08;
+}
+
+void buzzer_off (void)
+{
+	
+	PORTC=0x00;
+}
+
+
+//Function to Initialize PORTS
+void port_init()
+{
+	lcd_port_config();
+	adc_pin_config();
+	buzzer_pin_config();
+	motion_pin_config();	
+	servo1_pin_config();
+	servo2_pin_config();
+}
+
+// Timer 5 initialized in PWM mode for velocity control
+// Prescale:256
+// PWM 8bit fast, TOP=0x00FF
+// Timer Frequency:225.000Hz
+void timer5_init()
+{
+	TCCR5B = 0x00;	//Stop
+	TCNT5H = 0xFF;	//Counter higher 8-bit value to which OCR5xH value is compared with
+	TCNT5L = 0x01;	//Counter lower 8-bit value to which OCR5xH value is compared with
+	OCR5AH = 0x00;	//Output compare register high value for Left Motor
+	OCR5AL = 0xFF;	//Output compare register low value for Left Motor
+	OCR5BH = 0x00;	//Output compare register high value for Right Motor
+	OCR5BL = 0xFF;	//Output compare register low value for Right Motor
+	OCR5CH = 0x00;	//Output compare register high value for Motor C1
+	OCR5CL = 0xFF;	//Output compare register low value for Motor C1
+	TCCR5A = 0xA9;	/*{COM5A1=1, COM5A0=0; COM5B1=1, COM5B0=0; COM5C1=1 COM5C0=0}
+ 					  For Overriding normal port functionality to OCRnA outputs.
+				  	  {WGM51=0, WGM50=1} Along With WGM52 in TCCR5B for Selecting FAST PWM 8-bit Mode*/
+	
+	TCCR5B = 0x0B;	//WGM12=1; CS12=0, CS11=1, CS10=1 (Prescaler=64)
+}
+
+void adc_init()
+{
+	ADCSRA = 0x00;
+	ADCSRB = 0x00;		//MUX5 = 0
+	ADMUX = 0x20;		//Vref=5V external --- ADLAR=1 --- MUX4:0 = 0000
+	ACSR = 0x80;
+	ADCSRA = 0x86;		//ADEN=1 --- ADIE=1 --- ADPS2:0 = 1 1 0
+}
+
+//Function For ADC Conversion
+unsigned char ADC_Conversion(unsigned char Ch) 
+{
+	unsigned char a;
+	if(Ch>7)
+	{
+		ADCSRB = 0x08;
+	}
+	
+	Ch = Ch & 0x07;  			
+	ADMUX= 0x20| Ch;	   		
+	ADCSRA = ADCSRA | 0x40;		//Set start conversion bit
+	while((ADCSRA&0x10)==0);	//Wait for conversion to complete
+	a=ADCH;
+	ADCSRA = ADCSRA|0x10; //clear ADIF (ADC Interrupt Flag) by writing 1 to it
+	ADCSRB = 0x00;
+	return a;
+}
+
+//Function To Print Sesor Values At Desired Row And Coloumn Location on LCD
+void print_sensor(char row, char coloumn,unsigned char channel)
+{
+	
+	ADC_Value = ADC_Conversion(channel);
+	lcd_print(row, coloumn, ADC_Value, 3);
+}
+
+//Function for velocity control
+void velocity (unsigned char left_motor, unsigned char right_motor)
+{
+	OCR5AL = (unsigned char)left_motor;
+	OCR5BL = (unsigned char)right_motor;
+}
+
+//Function used for setting motor's direction
+void motion_set (unsigned char Direction)
+{
+ unsigned char PortARestore = 0;
+
+ Direction &= 0x0F; 		// removing upper nibbel for the protection
+ PortARestore = PORTA; 		// reading the PORTA original status
+ PortARestore &= 0xF0; 		// making lower direction nibbel to 0
+ PortARestore |= Direction; // adding lower nibbel for forward command and restoring the PORTA status
+ PORTA = PortARestore; 		// executing the command
+}
+
+void forward (void) 
+{
+  motion_set (0x06);
+}
+void back (void) //both wheels backward
+{
+	motion_set(0x09);
+}
+
+void left (void) //Left wheel backward, Right wheel forward
+{
+	motion_set(0x05);
+}
+
+void right (void) //Left wheel forward, Right wheel backward
+{
+	motion_set(0x0A);
+}
+
+void soft_left (void) //Left wheel stationary, Right wheel forward
+{
+	motion_set(0x04);
+}
+
+void soft_right (void) //Left wheel forward, Right wheel is stationary
+{
+	motion_set(0x02);
+}
+
+void soft_left_2 (void) //Left wheel backward, right wheel stationary
+{
+	motion_set(0x01);
+}
+
+void soft_right_2 (void) //Left wheel stationary, Right wheel backward
+{
+	motion_set(0x08);
+}
+
+void stop (void)
+{
+  motion_set (0x00);
+}
+void angle_rotate(unsigned int Degrees)
+{
+	float ReqdShaftCount = 0;
+	unsigned long int ReqdShaftCountInt = 0;
+
+	ReqdShaftCount = (float) Degrees/ 4.090; // division by resolution to get shaft count
+	ReqdShaftCountInt = (unsigned int) ReqdShaftCount;
+	ShaftCountRight = 0;
+	ShaftCountLeft = 0;
+
+	while (1)
+	{
+		if((ShaftCountRight >= ReqdShaftCountInt) | (ShaftCountLeft >= ReqdShaftCountInt))
+		break;
+	}
+	stop(); //Stop robot
+}
+
+
+
+void linear_distance_mm(unsigned int DistanceInMM)
+{
+	float ReqdShaftCount = 0;
+	unsigned long int ReqdShaftCountInt = 0;
+
+	ReqdShaftCount = DistanceInMM / 5.338; // division by resolution to get shaft count
+	ReqdShaftCountInt = (unsigned long int) ReqdShaftCount;
+	
+	ShaftCountRight = 0;
+	while(1)
+	{
+		if(ShaftCountRight > ReqdShaftCountInt)
+		{
+			break;
+		}
+	}
+	stop(); //Stop robot
+}
+
+void forward_mm(unsigned int DistanceInMM)
+{
+	forward();
+	linear_distance_mm(DistanceInMM);
+}
+
+void back_mm(unsigned int DistanceInMM)
+{
+	back();
+	linear_distance_mm(DistanceInMM);
+}
+
+void left_degrees(unsigned int Degrees)
+{
+	// 88 pulses for 360 degrees rotation 4.090 degrees per count
+	left(); //Turn left
+	angle_rotate(Degrees);
+}
+
+
+
+void right_degrees(unsigned int Degrees)
+{
+	// 88 pulses for 360 degrees rotation 4.090 degrees per count
+	right(); //Turn right
+	angle_rotate(Degrees);
+}
+
+
+void soft_left_degrees(unsigned int Degrees)
+{
+	// 176 pulses for 360 degrees rotation 2.045 degrees per count
+	soft_left(); //Turn soft left
+	Degrees=Degrees*2;
+	angle_rotate(Degrees);
+}
+
+void soft_right_degrees(unsigned int Degrees)
+{
+	// 176 pulses for 360 degrees rotation 2.045 degrees per count
+	soft_right();  //Turn soft right
+	Degrees=Degrees*2;
+	angle_rotate(Degrees);
+}
+
+void soft_left_2_degrees(unsigned int Degrees)
+{
+	// 176 pulses for 360 degrees rotation 2.045 degrees per count
+	soft_left_2(); //Turn reverse soft left
+	Degrees=Degrees*2;
+	angle_rotate(Degrees);
+}
+
+void soft_right_2_degrees(unsigned int Degrees)
+{
+	// 176 pulses for 360 degrees rotation 2.045 degrees per count
+	soft_right_2();  //Turn reverse soft right
+	Degrees=Degrees*2;
+	angle_rotate(Degrees);
+}
+
+void servo_1(unsigned char degrees)
+{
+	float PositionPanServo = 0;
+	PositionPanServo = ((float)degrees / 1.86) + 35.0;
+	OCR1AH = 0x00;
+	OCR1AL = (unsigned char) PositionPanServo;
+}
+
+
+//Function to rotate Servo 2 by a specified angle in the multiples of 1.86 degrees
+void servo_2(unsigned char degrees)
+{
+	float PositionTiltServo = 0;
+	PositionTiltServo = ((float)degrees / 1.86) + 35.0;
+	OCR1BH = 0x00;
+	OCR1BL = (unsigned char) PositionTiltServo;
+}
+
+void servo_1_free (void) //makes servo 1 free rotating
+{
+	OCR1AH = 0x03;
+	OCR1AL = 0xFF; //Servo 1 off
+}
+
+void servo_2_free (void) //makes servo 2 free rotating
+{
+	OCR1BH = 0x03;
+	OCR1BL = 0xFF; //Servo 2 off
+}
+
+void up (void) // grip the object
+{
+
+	servo_1 (10);
+	_delay_ms(1000);
+}
+
+void down (void) // relase the object
+{
+
+	servo_1 (90);
+	_delay_ms(1000);
+
+}
+void ungrip(void)
+{
+
+	servo_2(10);
+	_delay_ms(1000);
+}
+void grip(void)
+{
+	servo_2(145);
+	_delay_ms(1000);
+}
+
+void hand(void)
+{
+	stop();
+	_delay_ms(100);
+	buzzer_on();
+	_delay_ms(100);
+	buzzer_off();
+	_delay_ms(100);
+	forward();
+	_delay_ms(900);
+	left();
+	_delay_ms(1400);
+	stop();
+	_delay_ms(0);
+	forward();
+	_delay_ms(500);
+	stop();
+	_delay_ms(100);
+	down();
+	_delay_ms(500);
+	grip();
+	_delay_ms(500);
+	up();
+	_delay_ms(500);
+	down();
+	_delay_ms(500);
+	ungrip();
+	_delay_ms(500);
+	back();
+	_delay_ms(800);
+	stop();
+	_delay_ms(0);
+	right();
+	_delay_ms(1500);
+	stop();
+	_delay_ms(0);
+	forward();
+	_delay_ms(500);
+	stop();
+	_delay_ms(500);
+}
+void check(void)
+{
+	Front_IR_Sensor = ADC_Conversion(9);
+	flag1=0;
+	flag2=0;
+
+
+	print_sensor(2,8,9);	//Prints Value of Front IR Sensor
+	
+	if(ADC_Value>=76)
+	{
+		flag2=1;
+		hand();
+		
+	}
+	
+}
+
+void init_devices (void)
+{
+ 	cli(); //Clears the global interrupts
+	port_init();
+	adc_init();
+	timer5_init();
+	timer1_init();
+	sei();   //Enables the global interrupts
+}
+
+//Main Function
+int main()
+{
+	init_devices();
+	lcd_set_4bit();
+	lcd_init();
+	lcd_cursor(1,6);
+	lcd_string("WSR ROBOT");
+	while(1)
+	{
+		Left_white_line = ADC_Conversion(3);	//Getting data of Left WL Sensor
+		Center_white_line = ADC_Conversion(2);	//Getting data of Center WL Sensor
+		Right_white_line = ADC_Conversion(1);	//Getting data of Right WL Sensor
+	
+		flag=0;
+	
+
+		if(Center_white_line>0x28)
+		{
+			flag=1;
+			forward();
+			velocity(150,150);
+			check();
+		}
+
+		if((Left_white_line<0x28) || (flag==0))
+		{
+			flag=1;
+			forward();
+			velocity(130,50);
+			check();
+		}
+
+		if((Right_white_line<0x28) || (flag==0))
+		{
+			flag=1;
+			forward();
+			velocity(50,130);
+			check();
+		}
+
+		if(Center_white_line<0x28 || Left_white_line<0x28 || Right_white_line<0x28)
+		{
+			forward();
+			velocity(0,0);
+			check();
+		}
+
+	}
+}	
